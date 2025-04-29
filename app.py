10c5cc00dd98a2de8af6ffe2548b2d6e8f190900
febreze24555 <bpichon558@gmail.com>
2025-04-29 11:23:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -166,4 +166,6 @@ def delete_inscrit(inscrit_id):
     return redirect(url_for('admin'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
